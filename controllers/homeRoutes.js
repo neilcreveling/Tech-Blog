237b9f406f29980e059a96f233a3585eda166f0d
Neil Creveling <neil.creveling@gmail.com>
2021-05-03 19:54:43 -0400

    begin coding homeRoutes.js, userRoutes.js

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+    // serialize data
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
