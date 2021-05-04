f467eb3ab0beda944b4e077ab57836999ba50c91
Neil Creveling <neil.creveling@gmail.com>
2021-05-04 09:33:43 -0400

    create comment model

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,2 @@
+const { Comment } = require('../..models');
+const router = require('express').Router();
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,3 +24,21 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk
+    }
+})
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  module.exports = router;
