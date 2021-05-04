3a20efd3a27cfcb572e2736125e3c16d08da17c3
Neil Creveling <neil.creveling@gmail.com>
2021-05-04 11:40:44 -0400

    coded index.js model 2.0

--- models/index.js
+++ models/index.js
@@ -1,13 +1,35 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
     foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+    as: 'post_author',
 });
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    as: 'comment_author',
+})
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    as: 'post_author',
 });
 
-module.exports = { User, Post };
+Post.hasMany(Comment, {
+    foreignKey: 'comment_id',
+    as: 'post_comments'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    as: 'comment_author',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'comment_id',
+    as: 'blog_comments',
+});
+
+module.exports = { User, Post, Comment };
