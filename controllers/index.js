3a335359ec229828d7c3b66f94285297ec1cac81
Neil Creveling <neil.creveling@gmail.com>
2021-05-04 11:12:53 -0400

    coded index.js controller

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,15 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const postRoutes = require('./postRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
+const loginRoutes = require('./loginRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/post', postRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/login', loginRoutes);
+
 
 module.exports = router;
