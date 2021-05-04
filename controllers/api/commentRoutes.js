46f44c9374cce3fa230d9e75cc4c7081ca6e5fd6
Neil Creveling <neil.creveling@gmail.com>
2021-05-04 11:53:00 -0400

    coded commentRoutes.js, functionality for adding, updating, and deleting comments

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,6 +1,7 @@
 const { Comment } = require('../..models');
 const router = require('express').Router();
 
+// add comment
 router.post('/:user_id/:blog_id', async (req, res) => {
     try {
         const newComment = await Comment.create({
@@ -11,5 +12,38 @@ router.post('/:user_id/:blog_id', async (req, res) => {
         res.status(200).json(newComment);
     } catch (err) {
         res.status(500).json(err);
+    }
+});
+
+// update comment
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedComment = await Comment.update(
+            {
+                content: req.body.content
+            },
+            {
+                where: {
+                    id: req.params.id,
+                },
+            }
+        );
+        res.status(200).json(updatedComment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// delete comment
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedComment = await Comment.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        res.status(200).json(deletedComment);
+    } catch (err) {
+        res.status(400).json(err);
     }
 });
