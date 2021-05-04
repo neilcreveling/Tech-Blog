8276f038cf142c5075d791da6a2bb25ba29c0022
Neil Creveling <neil.creveling@gmail.com>
2021-05-04 12:00:50 -0400

    coded auth.js file

---
+++ utils/auth.js
@@ -0,0 +1,8 @@
+const withAuth = (req, res, next) => {
+    // when user isn't logged in, redirects to login page
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
