a564324210ff8d1750ebe40565ed744da5b0b886
Neil Creveling <neil.creveling@gmail.com>
2021-05-04 09:40:00 -0400

    coded logout functionality ro userRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -31,3 +31,13 @@ router.post('/login', async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end()
+        });
+    } else {
+        res.status(404).end();
+    }
+});
