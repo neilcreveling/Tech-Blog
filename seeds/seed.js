3f1894ebefb729e90d6dda33fe324149ad45beb3
Neil Creveling <neil.creveling@gmail.com>
2021-05-07 18:47:48 -0400

    finish seeding

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,12 @@
 const sequelize = require("../config/connection");
 const seedUsers = require("./userData.js");
-const seedBlogs = require("./blogData.js");
+const seedPosts = require("./postData.js");
 const seedComments = require("./commentData");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   await seedUsers();
-  await seedBlogs();
+  await seedPosts();
   await seedComments();
   process.exit(0);
 };
