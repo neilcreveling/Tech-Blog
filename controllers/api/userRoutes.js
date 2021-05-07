007815750e00967ea24a0ff28fede9920eba449c
Neil Creveling <neil.creveling@gmail.com>
2021-05-07 13:02:27 -0400

    edited userRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,29 +1,45 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.get("/", async (req, res) => {
+    try {
+      const allUsers = await User.findAll();
+      const userData = allUsers.map((user) => user.get({ plain: true }));
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 router.post('/', async (req, res) => {
     try {
-        const userData = await User.create(req.body);
-
+        const newUserData = await User.create({
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password,
+        });
         req.session.save(() => {
-            req.session.user_id = userData.id;
             req.session.logged_in = true;
-
-            res.status(200).json(userData);
+            req.session.username = newUserData.username;
+            req.session.user_id = newUserData.id;
+            res.status(200).json('homepage', {
+                logged_in: req.session.logged_in,
+                user_id: req.session.user_id,
+            });
         });
     } catch (err) {
-        res.status(400).json(err);
+        res.status(500).json(err);
     }
 });
 
 router.post('/login', async (req, res) => {
     try {
-        const userData = await User.findOne({ where: { username: req.body.username }});
+        const userData = await User.findOne({ where: { email: req.body.email } });
 
         if (!userData) {
             res
                 .status(400)
-                .json({ message: 'Incorrect username or password, please try again.'});
+                .json({ message: 'Incorrect email or password, please try again.'});
             return;
         }
 
@@ -39,8 +55,11 @@ router.post('/login', async (req, res) => {
         req.session.save(() => {
             req.session.user_id = userData.id;
             req.session.logged_in = true;
+            req.session.username = userData.username;
 
-            res.json({ user: userData, message: 'You are now logged in!' });
+            res
+            .status(200)
+            .json({ user: userData, message: 'You are now logged in!' });
         });
     } catch (err) {
         res.status(400).json(err);
