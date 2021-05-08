529ae568a869c69b0cbb2e1f624c18bf3044e4fb
Neil Creveling <neil.creveling@gmail.com>
2021-05-08 15:38:00 -0400

    coded public deletePost.js file

---
+++ public/js/deletePost.js
@@ -0,0 +1,19 @@
+const deletePost = async (event) => {
+  event.preventDefault();
+
+  console.log("hi delete");
+  const post_id = document.getElementById("delete-post").value;
+  console.log(post_id);
+
+  const response = await fetch(`/api/posts/${post_id}`, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert("Failed to delete post.");
+  }
+};
+
+document.getElementById("delete-post").addEventListener("click", deletePost);
